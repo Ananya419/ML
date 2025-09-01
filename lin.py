cc2a9c0f3cf12fef45189ef6647e64fa46f22e81
Ananya419 <shahiananya381@gmail.com>
2025-09-01 22:56:27 +0530

    add today's work

--- lin.py
+++ lin.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import Ridge, Lasso
 import math
 
 # read  .csv fileinto a dataframe
@@ -44,3 +45,44 @@ plt.show()
 
 # predicting the prices
 print("Prediction by the model: ",model.predict([[5560]]))
+
+
+
+ridge_model = Ridge(alpha=1.0)  # You can tune alpha
+ridge_model.fit(x, y)
+
+# Evaluate Ridge
+ridge_mse = mean_squared_error(y, ridge_model.predict(x))
+print("Ridge MSE:", math.sqrt(ridge_mse))
+print("Ridge R^2:", ridge_model.score(x, y))
+print("Ridge Coefficients:", ridge_model.coef_[0])
+print("Ridge Intercept:", ridge_model.intercept_[0])
+
+
+
+
+
+lasso_model = Lasso(alpha=0.1)  # You can tune alpha
+lasso_model.fit(x, y)
+
+# Evaluate Lasso
+lasso_mse = mean_squared_error(y, lasso_model.predict(x))
+print("Lasso MSE:", math.sqrt(lasso_mse))
+print("Lasso R^2:", lasso_model.score(x, y))
+print("Lasso Coefficients:", lasso_model.coef_)
+print("Lasso Intercept:", lasso_model.intercept_)
+
+
+
+
+
+
+plt.scatter(x, y, color='green', label='Data')
+plt.plot(x, model.predict(x), color='black', label='Linear')
+plt.plot(x, ridge_model.predict(x), color='blue', linestyle='--', label='Ridge')
+plt.plot(x, lasso_model.predict(x), color='red', linestyle=':', label='Lasso')
+plt.title("Regression Comparison")
+plt.xlabel("Area")
+plt.ylabel("Price")
+plt.legend()
+plt.show()
